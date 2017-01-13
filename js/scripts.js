f611ccfd7dbc10364f3a097b116af961aec09f81
saidahmed <ahmeds5@uw.edu>
2017-01-13 11:31:21 -0800

    add some jquery functionality, but will not be perfect.

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,21 @@
 
 
 
+var question1Input = $("input:radio[name=question1]:checked").val();
+var question2Input = $("input:radio[name=question2]:checked").val();
+var question3Input = $("input:radio[name=question3]:checked").val();
+var question4Input = $("input:radio[name=question4]:checked").val();
+var question5Input = $("input:radio[name=question5]:checked").val();
 
 
+if(question1Input === "bigcompany" || question5Input === ("csharpnet")) {
+  $("outputCSharp").slidein();
+} else if(question1Input==="startup" && question2Input==="yes" || question5Input==="rubyrails") {
+  $("outputRuby").slidein();
+} else if(question3Input==="yes" || )
 
 
+}
 
 
 
