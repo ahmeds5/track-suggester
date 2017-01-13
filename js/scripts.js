74476f0c79b175fa04f1c8ff5ff97e4555aa9f2b
saidahmed <ahmeds5@uw.edu>
2017-01-13 13:22:36 -0800

    will try to fix the jQuery functionality of showing reponses.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,22 @@
 //Business End
 
+var q1Input = $("input:radio[name=question1]:checked").val();
+var q2Input = $("input:radio[name=question2]:checked").val();
+var q3Input = $("input:radio[name=question3]:checked").val();
+var q4Input = $("input:radio[name=question4]:checked").val();
+var q5Input = $("input:radio[name=question5]:checked").val();
 
+var guesser = (function(q1Input, q2Input, q3Input, q4Input, q5Input) {
+  if(q1Input === "bigcompany" || q5Input === ("csharpnet")) {
+    (".CSharp").show();
+  } else if(q1Input==="startup" && q2Input==="yes" || q4Input=== "no" || q5Input==="rubyrails") {
+    (".Ruby").show();
+  } else if(q3Input==="yes" || q4Input==="yes" || q5Input==="javaandroid" ) {
+    (".Java").show();
+  } else {
 
-
-
-
-
-
-
-
-var question1Input = $("input:radio[name=question1]:checked").val();
-var question2Input = $("input:radio[name=question2]:checked").val();
-var question3Input = $("input:radio[name=question3]:checked").val();
-var question4Input = $("input:radio[name=question4]:checked").val();
-var question5Input = $("input:radio[name=question5]:checked").val();
-
-
-if(question1Input === "bigcompany" || question5Input === ("csharpnet")) {
-  $("outputCSharp").slidein();
-} else if(question1Input==="startup" && question2Input==="yes" || question5Input==="rubyrails") {
-  $("outputRuby").slidein();
-} else if(question3Input==="yes" || )
-
-
-}
-
-
-
-
-
-
-
-
-
-
+  };
+});
 
 
 
@@ -44,11 +27,7 @@ if(question1Input === "bigcompany" || question5Input === ("csharpnet")) {
 
 $(document).ready(function(){
   $("#blank").submit(function(event){
-    var question1Input = $("input:radio[name=question1]:checked").val();
-    var question2Input = $("input:radio[name=question2]:checked").val();
-    var question3Input = $("input:radio[name=question3]:checked").val();
-    var question4Input = $("input:radio[name=question4]:checked").val();
-    var question5Input = $("input:radio[name=question5]:checked").val();
+    $("#output").text(gueser);
 
     event.preventDefault();
 
