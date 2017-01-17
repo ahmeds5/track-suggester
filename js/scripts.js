cef58bd0769264b88930070807a08b08f125fb8c
saidahmed <ahmeds5@uw.edu>
2017-01-16 20:33:44 -0800

    add minor css syling changes and html changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,4 @@
-//Business End\
-
-// var q1Input = $("input:radio[name=question1]:checked").val();
-// var q2Input = $("input:radio[name=question2]:checked").val();
-// var q3Input = $("input:radio[name=question3]:checked").val();
-// var q4Input = $("input:radio[name=question4]:checked").val();
-// var q5Input = $("input:radio[name=question5]:checked").val();
-//
-// var guesser = (function(q1Input, q2Input, q3Input, q4Input, q5Input) {
-//   if(q1Input === "bigcompany" || q5Input === ("csharpnet")) {
-//     (".CSharp").show();
-//   } else if(q1Input==="startup"|| q4Input=== "no" || q5Input==="rubyrails") {
-//     (".Ruby").show();
-//   } else if(q3Input==="yes" || q4Input==="yes" || q5Input==="javaandroid" ) {
-//     (".outputJava").show();
-//   } else {
-//
-//   };
-// });
-
+//Business End
 
 //User End
 
