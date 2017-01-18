3272f71f9e8f087bda0059b6e6bd54e35d7cc0f0
saidahmed <ahmeds5@uw.edu>
2017-01-17 19:55:52 -0800

    add changes in javascript functionality and minor css styling changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 //Business End
 var guesser = function(q1Input, q2Input, q3Input, q4Input, q5Input) {
-  if(q1Input === "bigcompany" || q5Input === ("csharpnet")) {
-    $(".CSharp").show();
-  } else if(q1Input==="startup"|| q4Input=== "no" || q5Input==="rubyrails") {
-    $(".Ruby").show();
-  } else if(q3Input==="yes" || q4Input==="yes" || q5Input==="javaandroid" ) {
-    $(".outputJava").show();
+  if(q1Input === "bigcompany" && q5Input === ("csharpnet")) {
+    $(".CSharp").fadeIn();
+  } else if(q1Input==="startup"|| q4Input=== "no" && q5Input==="rubyrails") {
+    $(".Ruby").fadeIn();
+  } else if(q3Input==="yes" && q4Input==="yes" && q5Input==="javaandroid" ) {
+    $(".Java").fadeIn();
   } else {
 
   };
@@ -15,6 +15,7 @@ var guesser = function(q1Input, q2Input, q3Input, q4Input, q5Input) {
 
 $(document).ready(function(){
   $("#blank").submit(function(event){
+
     var q1Input = $("input:radio[name=question1]:checked").val();
     var q2Input = $("input:radio[name=question2]:checked").val();
     var q3Input = $("input:radio[name=question3]:checked").val();
@@ -23,11 +24,7 @@ $(document).ready(function(){
 
     $("#output").text(guesser(q1Input, q2Input, q3Input, q4Input, q5Input));
 
-
     event.preventDefault();
-    
-
 
   });
-
 });
